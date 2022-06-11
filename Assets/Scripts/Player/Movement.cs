c388f0933e4c890e88faa74584dc2e471a92e1f8
Bararda <anthonydoucet5417@hotmail.com>
2022-06-11 11:27:56 -0700

    player can only move horizontally

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -15,8 +15,6 @@ public class Movement : MonoBehaviour
     void HandleMovement()
     {
         float movementHorizontal = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
-        float movementVertical = Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
-        rigidbody2d.velocity = new Vector2(movementHorizontal, movementVertical);
-        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        rigidbody2d.velocity = new Vector2(movementHorizontal, 0);
     }
 }
