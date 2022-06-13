eef6bc6663ef04b05e61e743cf5527c12c76d465
Trevor LeBlanc <billbobphil@hotmail.com>
2022-06-12 20:48:58 -0700

    Fixed solution and project file. Removed mass multiplication in external force scripts. Added constant force script and component to player.

--- Assets/Scripts/Interactables/ApplyConstantForceToPlayer.cs
+++ Assets/Scripts/Interactables/ApplyConstantForceToPlayer.cs
@@ -1,8 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Enums;
 using UnityEngine;
 
-public class ApplyConstantForceToPlayer : MonoBehaviour
+namespace Interactables
 {
+    public class ApplyConstantForceToPlayer : MonoBehaviour
+    {
+        private ConstantForce2D _playerConstantForce;
+        public float externalForce;
+        public ExternalMovePlayerDirection forceDirection;
+        private const ForceMode2D ForceMode = ForceMode2D.Impulse;
+        
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            _playerConstantForce = col.GetComponent<ConstantForce2D>();
+        }
+        
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (!other.CompareTag("Player")) return;
     
+            switch (forceDirection)
+            {
+                case ExternalMovePlayerDirection.Upwards:
+                    _playerConstantForce.force = new Vector2(0, externalForce);
+                    break;
+                case ExternalMovePlayerDirection.Left:
+                    _playerConstantForce.force = new Vector2(-externalForce, 0);
+                    break;
+                case ExternalMovePlayerDirection.Right:
+                    _playerConstantForce.force = new Vector2(externalForce, 0);
+                    break;
+                case ExternalMovePlayerDirection.Downwards:
+                    _playerConstantForce.force = new Vector2(0, -externalForce);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
 }
