f50ea44ea091b29fa3638aabb8f32f5cadc2fd4e
Trevor LeBlanc <billbobphil@hotmail.com>
2022-06-16 17:01:44 -0700

    Added player art with animation

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -7,6 +7,15 @@ namespace Player
         public Rigidbody2D rigidbody2d;
         public float speed = 100f;
         public float maxSpeed = 50f;
+        private Animator _animator;
+        private static readonly int IsHoldingDownKey = Animator.StringToHash("isHoldingDownKey");
+        private SpriteRenderer _spriteRenderer;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
 
         private void FixedUpdate()
         {
@@ -20,11 +29,19 @@ namespace Player
                 // multiply be mass so that the movement is not affected by the mass of the object
                 rigidbody2d.AddForce(-transform.right * (speed * rigidbody2d.mass), ForceMode2D.Force);
                 rigidbody2d.velocity = new Vector2(Mathf.Clamp(rigidbody2d.velocity.x, -maxSpeed, 0f), rigidbody2d.velocity.y);
+                _animator.SetBool(IsHoldingDownKey, true);
+                _spriteRenderer.flipX = true;
             }
             else if (Input.GetKey(KeyCode.D))
             {
                 rigidbody2d.AddForce(transform.right * (speed * rigidbody2d.mass), ForceMode2D.Force);
                 rigidbody2d.velocity = new Vector2(Mathf.Clamp(rigidbody2d.velocity.x, 0f, maxSpeed), rigidbody2d.velocity.y);
+                _animator.SetBool(IsHoldingDownKey, true);
+                _spriteRenderer.flipX = false;
+            }
+            else
+            {
+                _animator.SetBool(IsHoldingDownKey, false);
             }
 
         }
